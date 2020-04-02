187ef49d6b7fe0fd6477ed4a02810a338b5db7da
AdmyHusky <net_kanet@hotmail.com>
2020-04-02 22:21:44 +0700

    Home page

--- fontend/src/views/Home.vue
+++ fontend/src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <!-- Top  -->
+    <v-app-bar app color="cyan" dark>
+      <v-toolbar-title class="mr-2" style="margin-left: 5px;">English_Evening</v-toolbar-title>
+      <v-spacer />
+      <v-btn text>
+        <v-icon>mdi-login</v-icon>
+        <span class="mr-2" style="margin-left: 5px;">Sign in</span>
+      </v-btn>
+    </v-app-bar>
+    <!--Center -->
+      <div class="fill-height" fluid>
+        <div class="row">
+            <v-img
+              alt="Wallpaper"
+              contain
+              :src="require('../assets/e-learning.jpg')"
+              transition="scale-transition"
+            />
+        </div>
+      </div>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
 </script>
