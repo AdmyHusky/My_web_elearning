7c27e734bf49d9478d70eada1869b9fea5342138
AdmyHusky <net_kanet@hotmail.com>
2020-04-06 04:18:30 +0700

    test add v -card

--- fontend/src/views/Home.vue
+++ fontend/src/views/Home.vue
@@ -3,10 +3,43 @@
     <!-- Menu Bar  -->
     <BarLogin />
     <!--image background -->
-    <div class="row">
+    <div class="background">
       <v-img id="image" alt="Wallpaper" :src="require('../assets/e-learning.jpg')" />
     </div>
-    <div class="fill-height" fluid></div>
+    <!-- Page content -->
+    <v-content>
+      <v-container class="fill-height" fluid>
+    <!-- Course Section -->
+        <v-row no-gutters>
+          <v-col cols="12" sm="1">
+            <h1>Course</h1>
+          </v-col>
+        </v-row>
+    <!-- Course  -->  
+     <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+          ></v-img>
+  
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div> {{ card_text }} </div>
+            </div>
+          </v-card-title>
+  
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
@@ -34,9 +67,12 @@ export default {
 
 <style scoped>
 #image {
-  height: 510px;
+  height: 550px;
   width: 2000px;
   filter: blur(1px);
   filter: brightness(75%);
+}
+.content {
+  max-width: 1564px;
 }
 </style>
